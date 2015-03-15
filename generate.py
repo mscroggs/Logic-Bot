bd1f6cc5bead6c7522853a5d4946d0a6a3f02368
MScroggs <matthew.w.scroggs@gmail.com>
2015-03-15 22:14:09 +0000

    Re-wrote much more efficiently

--- generate.py
+++ generate.py
@@ -1,22 +1,72 @@
 from math import floor
 from time import time
 
-def base_convert(n_10,base):
-    if n_10<base:
-        return [int(n_10)]
-    digit = n_10 % base
-    n_10 -= digit
-    n_10 /= base
-    return base_convert(n_10,base)+[int(digit)]
+class Symbols:
+    def __init__(self):
+        self.variables_dummy = ["a"]
+        self.variables = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                     "n","o","p","q","r","s","t","u","v","w","x","y","z",
+                     "A","B","C","D","E","F","G","H","I","J"]
+        self.binary = [">","=","/","+"]
+        self.unary = ["-"]
+        self.first = ["("]+self.unary
+        self.back_first = {}
+        for i,val in enumerate(self.first):
+            self.back_first[val]=i
+        
+        self.after = {}
+        self.backw = {}
+        for v in self.variables:
+            self.after[v] = [")"]+self.binary
+        for b in self.binary:
+            self.after[b] = self.unary+self.variables_dummy
+        for u in self.unary:
+            self.after[u] = self.unary+self.variables_dummy
+        self.after["("] = ["("]+self.unary+self.variables_dummy
+        self.after[")"] = [")"]+self.binary
+        
+        for a in self.after:
+            self.backw[a]={}
+            for i,b in enumerate(self.after[a]):
+                self.backw[a][b] = i
 
-def in_order(list):
-    if len(list)>0:
-        if list[0]!=7:
-            return False
-        for i,val in enumerate(list[:-1]):
-            if val!=list[i+1]-1:
-                return False
-    return True
+class Formula:
+    def __init__(self):
+        self.S = Symbols()
+        self.choice = [self.S.first]
+        self.back = [self.S.back_first]
+        self.current = [0]
+
+    def next(self):
+        i = 0
+        while i<len(self.current):
+            self.current[i]+=1
+            if self.current[i]<len(self.choice[i]):
+                break
+            else:
+                self.current[i] = 0
+            i+=1
+        if i == len(self.current):
+            self.current += [0]
+            self.choice += [self.S.first]
+            self.back += [self.S.back_first]
+        i-=1
+        while i>=0:
+            self.choice[i] = self.S.after[self[i+1]]# or -i-2
+            self.back[i] = self.S.backw[self[i+1]]
+            if "a" in self.choice[i]:
+                pass
+            # update back and choice
+            i-=1
+
+    def __str__(self):
+        output = ""
+        for i,val in enumerate(self.current):
+            output = self.choice[i][val]+output
+        return output
+
+    def __getitem__(self,i):
+        return self.choice[i][self.current[i]]
 
 def logic_iterate(test):
     test = "1".join(test.split("-0"))
@@ -45,74 +95,41 @@ def logic_iterate(test):
 
 with open("tautologies","w") as f:
     f.write("# KEY\n")
-    f.write("# - not\n")
-    f.write("# + and\n")
-    f.write("# / or\n")
-    f.write("# > implies\n")
-    f.write("# = if and only if \n")
-    f.write("# a-z and A-J represent variables \n")
+    f.write("#  - not\n")
+    f.write("#  + and\n")
+    f.write("#  / or\n")
+    f.write("#  > implies\n")
+    f.write("#  = if and only if \n")
+    f.write("#  a-z and A-J represent variables \n\n")
 with open("sequence","w") as f:
     pass
 
-variables = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
-             "n","o","p","q","r","s","t","u","v","w","x","y","z",
-             "A","B","C","D","E","F","G","H","I","J"]
-
-charray = [")","(",">","=","/","+","-"]+variables
-
 found = 0
 leng = 1
-i=0
-next_print = 10**7
-start = time()
-while i<43**141:
-    conv = base_convert(i,43)
-    if len(conv)>leng:
-        leng = len(conv)
+formula = Formula()
+while True:
+    current = str(formula)
+    if len(current)>leng:
+        leng = len(current)
         with open("sequence","a") as f:
             f.write(str(found)+",")
         found = 0
-    formula = ""
-    num_order=[]
-    for num in conv:
-        formula += charray[num]
-        if num>=7 and num not in num_order:
-            num_order.append(num)
-    if in_order(num_order):
-        tautology = True
-        for j in range(0,2**len(num_order)):
-            assignment = base_convert(j,2)
-            while len(assignment)<len(num_order):
-                assignment = [0]+assignment
-            test = formula
-            for k,val in enumerate(assignment):
-                test = str(val).join(test.split(variables[k]))
-            oldtest = "Z"
-            while oldtest != test:
-                oldtest = test
-                test = logic_iterate(test)
-            if test != "1":
-                tautology = False
-                break
-        if tautology:
-            found += 1
-            #formula=formula.replace('-',u'\u00AC')
-            #formula=formula.replace('>',u'\u21FE')
-            #formula=formula.replace('=',u'\u21FF')
-            #formula=formula.replace('+',u'\u2227')
-            #formula=formula.replace('/',u'\u2228')
-            with open("tautologies","a") as f:
-                f.write(formula+"\n")
-            print(formula)
-    added = False
-    for j,character in enumerate(conv):
-        if (character>(j-1)/3+7 or 
-            (j==1 and conv[0] in [0,2,3,4,5])
-            ):
-            dig = 43**(len(conv)-j)
-            i -= i % dig
-            i += dig
-            added = True
+    tautology = True
+    for j in range(0,2**len(current)):
+        assignment = base_convert(j,2)
+        assignment = [0]*(len(current)-len(assignment))+assignment
+        test = current
+        for k,val in enumerate(assignment):
+            test = str(val).join(test.split(variables[k]))
+        oldtest = "Z"
+        while oldtest != test:
+            oldtest = test
+            test = logic_iterate(test)
+        if test != "1":
+            tautology = False
             break
-    if not added:
-        i+=43
+    if tautology:
+        found += 1
+        with open("tautologies","a") as f:
+            f.write(current+"\n")
+        print(current)
