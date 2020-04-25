a5a1ed7756ecf9e15a539b1af22a3c845e4568ac
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-04-25 19:37:49 +0100

    file test

--- test/test_twitter.py
+++ test/test_twitter.py
@@ -16,3 +16,14 @@ def test_twitter():
     fac.formula.as_unicode()
     print("Time taken: " + str(time() - start) + "s")
     assert fac.formula.as_ascii() == "(a/(a>-(a+a)))"
+
+
+def test_file_read_write():
+    fac = FormulaFactory()
+    fac.set_ascii("(a/-a)")
+    with open("formula_test", "w") as f:
+        f.write(fac.formula.as_ascii())
+
+    with open("formula_test") as f:
+        fac.set_ascii(f.read())
+    fac.next()
