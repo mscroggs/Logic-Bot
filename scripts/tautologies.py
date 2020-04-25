488b27720b045b38f4b3238b0ce9a4ab132a435a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-04-25 10:25:52 +0100

    update tautologies

--- scripts/tautologies.py
+++ scripts/tautologies.py
@@ -1,33 +1,35 @@
-from logic_core import Formula
+from logic import FormulaFactory
 import sys
-f = Formula()
-f.next()
 
-if len(sys.argv)>=2:
-    with open(sys.argv[1],"w") as file:
-        file.write("# KEY
-\n#  - not
-\n#  + and
-\n#  / o
-\n#  > implies
-\n#  = if and only if
-\n#  a-z and A-J represent variables
-\n")
+n = None
+filename = None
+for option, value in zip(sys.argv[1::2], sys.argv[2::2]):
+    if option == "--file" or option == "-f":
+        filename = value
+    if option == "--max_number" or option == "-n":
+        n = int(value)
+
+fac = FormulaFactory()
+fac.next()
+
+if filename is not None:
+    with open(filename, "w") as f:
+        f.write(fac.symbols.ascii_key() + "\n")
 
 count = 0
 leng = 1
 seq = []
 
 try:
-    while True:
-        if f.is_tautology():
+    while n is None or len(fac.formula) <= n:
+        if fac.formula.is_tautology():
             count += 1
-            print(f)
-            if len(sys.argv)>=2:
-                with open(sys.argv[1],"a") as file:
-                    file.write("\n"+f.as_ascii())
-        f.next()
-        while leng < len(f):
+            print(fac.formula)
+            if filename is not None:
+                with open(sys.argv[1], "a") as file:
+                    file.write(fac.formula.as_ascii() + "\n")
+        fac.next()
+        while leng < len(fac.formula):
             seq.append(count)
             leng += 1
             count = 0
